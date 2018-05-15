30c7624d67d2b392660efd851711ffa05b6e2ad4
jaeyeoupjung <wp01053@naver.com>
2018-05-15 12:01:18 +0900

    dog-final

--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,38 @@
           <h2 class="subtitle">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>
         </div>
       </div>
-    </div>  
-    <br>  
-      <div class="columns">
-        <div class="column">        
-         <div class="notification is-info">
+    </div>
+    <br>
+    <div class="columns">
+      <div class="column">
+        <div class="notification is-info">
+          <figure class="image is-squre">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg" alt="">
+          </figure>
+        </div>
+      </div>
+      <div class="column">
+        <div class="message is-info">
+          <div class="message-header">
+            <p>오늘의 개 사진</p>
+          </div>
+          <div class="message-body">
             <figure class="image is-squre">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg" alt="">
+              <img v-bind:src="image" alt="">
             </figure>
           </div>
         </div>
-        <div class="column">         
-         <div class="message is-info">
-           <div class="message-header">
-             <p>오늘의 개 사진</p>
-           </div>
-           <div class="message-body">
-             <figure class="image is-squre">
-               <img v-bind:src="image" alt="">
-             </figure>
-           </div>
-         </div>
-        </div>
       </div>
-    </section>
+    </div>
+    <div class="box">
+      <h1 class="title">보고싶은 개 정보를 누르세요.</h1>
+      <span v-for="dog in dogs" v-bind:key="dog">
+      <a href="#" class="button is-success">
+        {{dog}}
+      </a>&nbsp;
+    </span>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -41,7 +49,8 @@
     },
     async asyncData() {
       const myImage = await axios.get('https://dog.ceo/api/breeds/image/random');
-      return { image: myImage.data.message };
+      const myDogs = await axios.get('https://dog.ceo/api/breeds/list');
+      return { image: myImage.data.message, dogs: myDogs.data.message };
     }
   };
 </script>
