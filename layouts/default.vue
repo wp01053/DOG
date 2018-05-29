dc8c3fe921f63efff53b40a19ab0fadda239c722
jaeyeoupjung <wp01053@naver.com>
2018-05-29 12:54:03 +0900

    2018-05-29 dogfinal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <nav class="navbar is-warning ">
+      <div class="navbar-barnd ">
+        <nuxt-link to="" class="navbar-item">개사랑</nuxt-link>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
+        </div>
+      </div>
+    </nav>
     <nuxt/>
+    <footer class="footer has-text-contered">
+      <p>2018년 목원대학교 개사랑. 모든 저작권은 정재엽에게 있습니다. </p>
+
+    </footer>
   </div>
+
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="section">
+        <article class="hero is-info">
+            <div class="hero-class"></div>
+        </article>
+    </section>
+</template>
