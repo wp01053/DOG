154b29f1222345e73adfb84ca87a578bf000698d
jaeyeoupjung <wp01053@naver.com>
2018-05-29 11:10:53 +0900

    2018-05-29 dog2

--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -1,7 +1,12 @@
 <template>
 <section class="section">
-    <article class="hero">
-        <div class="hero-body"></div>
+    <article class="hero is info">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">저는 {{dogName.toUpperCase()}}입니다.</h1>
+                <h2 class="subtitle">많이 사랑해 주세욤!</h2>
+            </div>
+        </div>
 
 
     </article>
