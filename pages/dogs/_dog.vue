735ad8485d8d7320dca24cb0d91032fdfcfb6d6b
jaeyeoupjung <wp01053@naver.com>
2018-05-29 12:22:20 +0900

    2018-05-29 dog5

--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -18,6 +18,18 @@
 
             </div>
         </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                <p>{{dogName.toUpperCase()}}의 아품종</p>
+            </div>
+            <div class="message-body">
+                <ol>
+                    <li v-for="subdog in dogList" :key="subdog">
+                        {{subdog.toUpperCase()}}
+                    </li>
+                </ol>
+            </div>
+        </article>
     </section>
 </template>
 <script>
@@ -30,9 +42,11 @@
                 const image = await axios.get(`https://dog.ceo/api/breed/${params.dog}/images/random`);
                 images.push(image.data.message);
             }
+            const list = await axios.get(`https://dog.ceo/api/breed/${params.dog}/list`);
             return {
                 dogName: params.dog,
-                images: images
+                images: images,
+                dogList: list.data.message
             };
         }
     };
