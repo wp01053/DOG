e997f78f2bc4bb81cd68a6a1db8d792d39af4f8a
jaeyeoupjung <wp01053@naver.com>
2018-06-05 09:51:09 +0900

    [2018-06-05] dog-1

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,41 +3,64 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'dog',
+    title: "개사랑",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "대한민국 애견인의 개사랑"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css' }
-    
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://uicdn.toast.com/tui.chart/latest/tui-chart.css"
+      }
     ],
-    script: [{ src: 'https://use.fontawesome.com/releases/v5.0.7/js/all.js', defer: '' }]
+    script: [
+      {
+        src: "https://use.fontawesome.com/releases/v5.0.7/js/all.js",
+        defer: ""
+      },
+      {
+        src:
+          "https://uicdn.toast.com/tui.code-snippet/latest/tui-code-snippet.js"
+      },
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.7/raphael.js"
+      },
+      { src: "https://uicdn.toast.com/tui.chart/latest/tui-chart.js" }
+    ]
   },
-
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
   build: {
-    vendor: ['axios'],
+    vendor: ["axios"],
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
---
+++ pages/stat.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="section">
+        <article class="hero is-info">
+            <div class="hero_body">
+                <h1 class="title">애견통계</h1>
+            </div>
+        </article>
+        <article class="message">
+            <div class="message-head">
+                애견 산업 전망
+            </div>
+            <div class="message-body">
+                <div id="charArea"></div>
+            </div>
+        </article>
+    </section>
+</template>
